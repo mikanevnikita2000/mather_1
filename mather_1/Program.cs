24482c425d91d0f54aeddc00135a604c8b98071d
mikanevnikita2000 <86831041+mikanevnikita2000@users.noreply.github.com>
2021-10-04 21:44:01 +0300

    5

--- mather_1/Program.cs
+++ mather_1/Program.cs
@@ -1,31 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace mather_1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             bool isEnabled = true;
             while (isEnabled)
             {
                 Console.WriteLine("Выбери какие примеры тебе нужны:");
-                Console.WriteLine("1. Сложение");
+                Console.WriteLine("1. Сложение ");
                 Console.WriteLine("2. Вычитание");
                 Console.WriteLine("3. Умножение");
                 Console.WriteLine("4. Деление");
                 Console.WriteLine("5. Выход");
-                string num = Console.ReadLine();
+                int num = Int32.Parse(Console.ReadLine());
+                isEnabled = AdditionAndSubtractionAndMultiplication(num);
             }
         }
-        static bool AdditionAndSubtractionAndMultiplication(string num, bool isEnabled)
+        static bool AdditionAndSubtractionAndMultiplication(int num)
         {
-            int expectedResult = 0;
-            int exampleResponse;
+            string visibleExpression;
+            bool isEnabled = true;
+            int expectedResult;           
             Random gpg = new Random();
             int a = gpg.Next(100);
             int b = gpg.Next(100);
@@ -33,35 +31,46 @@ namespace mather_1
             bool isHided = true;
             while (isHided)
             {
-                if (num == "1")
+                if (num == 1)
                 {
-                    if (expectedResult == a + b)
+                    visibleExpression = ($"{a} + {b}");
+                    expectedResult = a + b;
+                    test(expectedResult, visibleExpression);
+                }
+                if (num == 2)
+                {
+                    visibleExpression = ($"{a} - {b}");
+                    expectedResult = a - b;
+                    test(expectedResult, visibleExpression);
+                }
+                if (num == 3)
+                {
+                    visibleExpression = ($"{a} * {b}");
+                    expectedResult = a * b;
+                    test(expectedResult, visibleExpression);
+                }
+                if (num == 4)
+                {
+                    int[,] loc =
                     {
-                        Console.WriteLine($"сколько будет: {a} + {b}");
-                        exampleResponse = a + b;
-                    }
+                        { 1, 2, 3, 4, 5, 6, 7, 8, 9},
+                        { 4, 6, 8, 10, 12, 14, 16, 18, 20 },
+                        { 6, 9, 12, 15, 18, 21, 24, 28, 30 },
+                        { 8, 12, 16, 20, 24, 28, 32, 36, 40 },
+                        { 10, 15, 20, 25, 30, 35, 40, 45, 50 },
+                        { 12, 18, 24, 30, 36, 42, 48, 54, 60 },
+                        { 14, 21, 28, 35, 42, 49, 56, 63, 70 },
+                        { 16, 24, 32, 40, 48, 56, 64, 72, 80 },
+                        { 18, 27, 36, 45, 54, 63, 72, 81, 90 },
+                    };
+                    int delimoe;
+                    int generetdelitel = gpg.Next(0, 9);
+                    delimoe = loc[gpg.Next(0, 9), gpg.Next(0, 9)];
+                    visibleExpression = ($"{delimoe} / {generetdelitel}");
+                    expectedResult = delimoe / generetdelitel;
+                    test(expectedResult, visibleExpression);
                 }
-                if (num == "2")
-                {
-                    if (expectedResult == a - b)
-                    {
-                        Console.WriteLine($"сколько будет: {a} - {b}");
-                        exampleResponse = a - b;
-                    }
-                }
-                if (num == "3")
-                {
-                    if (expectedResult == a * b)
-                    {
-                        Console.WriteLine($"сколько будет: {a} * {b}");
-                        exampleResponse = a * b;
-                    }
-                }
-                if (num == "4")
-                {
-
-                }
-                if (num == "5")
+                if (num == 5)
                 {
                     isEnabled = false;
                 }
@@ -74,7 +83,7 @@ namespace mather_1
             }
             return isEnabled;
         }
-        static bool test(string visibleExpression, int expectedResult)
+        static void test( int expectedResult, string visibleExpression)
         {
             bool result = false;
             Console.WriteLine($"сколько будет: {visibleExpression}");
@@ -90,77 +99,6 @@ namespace mather_1
                     Console.WriteLine("попробуй ещё раз");
                 }
             }
-            return result;
-        }
-        static bool TheDivisionMethod(bool isEnabled, bool isCorrect)
-        {
-            while (isEnabled == false)
-            {
-                int[,] loc =
-                {
-                { 1, 2, 3, 4, 5, 6, 7, 8, 9},
-                { 4, 6, 8, 10, 12, 14, 16, 18, 20 },
-                { 6, 9, 12, 15, 18, 21, 24, 28, 30 },
-                { 8, 12, 16, 20, 24, 28, 32, 36, 40 },
-                { 10, 15, 20, 25, 30, 35, 40, 45, 50 },
-                { 12, 18, 24, 30, 36, 42, 48, 54, 60 },
-                { 14, 21, 28, 35, 42, 49, 56, 63, 70 },
-                { 16, 24, 32, 40, 48, 56, 64, 72, 80 },
-                { 18, 27, 36, 45, 54, 63, 72, 81, 90 },
-                };
-                Random gpg = new Random();
-                int exampleResponse;
-                int delimoe;
-                int generetdelitel = gpg.Next(0, 9);
-                int generetdelimoe = gpg.Next(0, 9);
-                if (generetdelitel == 0)
-                {
-                    delimoe = loc[0, generetdelimoe];
-                    exampleResponse = delimoe / generetdelitel;
-                }
-                if (generetdelitel == 1)
-                {
-                    delimoe = loc[1, generetdelimoe];
-                    exampleResponse = delimoe / generetdelitel;
-                }
-                if (generetdelitel == 2)
-                {
-                    delimoe = loc[2, generetdelimoe];
-                    exampleResponse = delimoe / generetdelitel;
-                }
-                if (generetdelitel == 3)
-                {
-                    delimoe = loc[3, generetdelimoe];
-                    exampleResponse = delimoe / generetdelitel;
-                }
-                if (generetdelitel == 4)
-                {
-                    delimoe = loc[4, generetdelimoe];
-                    exampleResponse = delimoe / generetdelitel;
-                }
-                if (generetdelitel == 5)
-                {
-                    delimoe = loc[5, generetdelimoe];
-                    exampleResponse = delimoe / generetdelitel;
-                }
-                if (generetdelitel == 6)
-                {
-                    delimoe = loc[6, generetdelimoe];
-                    exampleResponse = delimoe / generetdelitel;
-                }
-                if (generetdelitel == 7)
-                {
-                    delimoe = loc[7, generetdelimoe];
-                    exampleResponse = delimoe / generetdelitel;
-                }
-                if (generetdelitel == 8)
-                {
-                    delimoe = loc[8, generetdelimoe];
-                    exampleResponse = delimoe / generetdelitel;
-                }
-                int expectedResult = (Convert.ToInt32(Console.ReadLine()));
-            }
-                return isEnabled;
         }
     }
 }
